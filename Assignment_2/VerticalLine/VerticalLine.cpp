e1f4e4b08f459a2ea27f67701fe0f2239023480c
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-10-15 18:14:53 +0300

    added/updated declaration and implementation of vertical line class

--- Assignment_2/VerticalLine/VerticalLine.cpp
+++ Assignment_2/VerticalLine/VerticalLine.cpp
@@ -3,7 +3,19 @@
 VerticalLine::VerticalLine(int x, int y, int length) : x(x), y(y), length(length) {}
 
 void VerticalLine::draw(Board &board) {
-    for(int i = y; i < y + length && i < BoardHeight; i++) {
-        board.setGrid(i, x);
+    if (x >= 0 && x < BoardWidth && y >= 0 && y < BoardHeight) {
+        for (int i = y; i < y + length && i < BoardHeight; i++) {
+            board.setGrid(i, x);
+        }
+    } else {
+        return;
     }
 }
+
+void VerticalLine::printInfo() {
+    std::cout << x << " " << y << " " << length << std::endl;
+}
+
+
+
+
--- Assignment_2/VerticalLine/VerticalLine.h
+++ Assignment_2/VerticalLine/VerticalLine.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "../Shape/Shape.h"
+#include "../Board/Board.h"
 
-class VerticalLine : Shape {
+class VerticalLine : public Shape {
 private:
     int x, y, length;
 public:
     VerticalLine(int x, int y, int length);
-    ~VerticalLine() = default;
+    ~VerticalLine() override = default;
     void draw(Board &board) override;
+    void printInfo() override;
 };
 
