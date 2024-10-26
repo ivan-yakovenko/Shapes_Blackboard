95d0824558769d4cac1610984ba33a5e11c279c2
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-10-26 21:18:47 +0300

    added new implementation/declaration for add method to fill and color the rectangle

--- src/Rectangle/Rectangle.cpp
+++ src/Rectangle/Rectangle.cpp
@@ -1,27 +1,84 @@
 #include "Rectangle.h"
 
-Rectangle::Rectangle(int x, int y, int width, int height)
-        : x(x), y(y), width(width), height(height) {}
+Rectangle::Rectangle(int x, int y, bool isFilled, std::string &color, bool colorFramed, std::string &color2, int width,
+                     int height)
+        : Shape(x, y, isFilled, color, colorFramed, color2), width(width), height(height) {}
 
-void Rectangle::draw(Board &board) {
+void Rectangle::draw(Board &board, bool isFilled, std::string color, bool colorFramed, std::string color2) {
     if (x >= 0 && x < BoardWidth && y >= 0 && y < BoardHeight) {
-        HorizontalLine topLine(x, y, width);
-        VerticalLine leftLine(x, y, height);
-        VerticalLine rightLine(x + width - 1, y, height);
-        HorizontalLine bottomLine(x, y + height - 1, width);
 
-        topLine.draw(board);
-        leftLine.draw(board);
-        rightLine.draw(board);
-        bottomLine.draw(board);
-    }
-    else {
+        if (isFilled && !color.empty() && colorFramed && !color2.empty()) {
+            char c = color[0];
+            c = static_cast<char>(std::toupper(c));
+
+            HorizontalLine topLine(x, y, isFilled, color, colorFramed, color2, width);
+            VerticalLine leftLine(x, y, isFilled, color, colorFramed, color2, height);
+            VerticalLine rightLine(x + width - 1, y, isFilled, color, colorFramed, color2, height);
+            HorizontalLine bottomLine(x, y + height - 1, isFilled, color, colorFramed, color2, width);
+
+            topLine.draw(board, isFilled, color, colorFramed, color2);
+            leftLine.draw(board, isFilled, color, colorFramed, color2);
+            rightLine.draw(board, isFilled, color, colorFramed, color2);
+            bottomLine.draw(board, isFilled, color, colorFramed, color2);
+
+            for (int i = y + 1; i < y + height - 1; i++) {
+                for (int j = x + 1; j < x + width - 1; j++) {
+                    board.setGrid(i, j, c);
+                }
+            }
+        }
+
+
+        if (colorFramed && !color2.empty()) {
+            HorizontalLine topLine(x, y, isFilled, color, colorFramed, color2, width);
+            VerticalLine leftLine(x, y, isFilled, color, colorFramed, color2, height);
+            VerticalLine rightLine(x + width - 1, y, isFilled, color, colorFramed, color2, height);
+            HorizontalLine bottomLine(x, y + height - 1, isFilled, color, colorFramed, color2, width);
+
+            topLine.draw(board, isFilled, color, colorFramed, color2);
+            leftLine.draw(board, isFilled, color, colorFramed, color2);
+            rightLine.draw(board, isFilled, color, colorFramed, color2);
+            bottomLine.draw(board, isFilled, color, colorFramed, color2);
+        }
+
+        if (isFilled && !color.empty()) {
+            char c = static_cast<char>(std::toupper(color[0]));
+
+            HorizontalLine topLine(x, y, isFilled, color, colorFramed, color, width);
+            VerticalLine leftLine(x, y, isFilled, color, colorFramed, color, height);
+            VerticalLine rightLine(x + width - 1, y, isFilled, color, colorFramed, color, height);
+            HorizontalLine bottomLine(x, y + height - 1, isFilled, color, colorFramed, color, width);
+
+            topLine.draw(board, isFilled, color, colorFramed, color2);
+            leftLine.draw(board, isFilled, color, colorFramed, color2);
+            rightLine.draw(board, isFilled, color, colorFramed, color2);
+            bottomLine.draw(board, isFilled, color, colorFramed, color2);
+
+            for (int i = y + 1; i < y + height - 1; i++) {
+                for (int j = x + 1; j < x + width - 1; j++) {
+                    board.setGrid(i, j, c);
+                }
+            }
+        }
+        if(color.empty() && color2.empty()) {
+            HorizontalLine topLine(x, y, isFilled, color, colorFramed, color2, width);
+            VerticalLine leftLine(x, y, isFilled, color, colorFramed, color2, height);
+            VerticalLine rightLine(x + width - 1, y, isFilled, color, colorFramed, color2, height);
+            HorizontalLine bottomLine(x, y + height - 1, isFilled, color, colorFramed, color2, width);
+
+            topLine.draw(board, isFilled, color, colorFramed, color2);
+            leftLine.draw(board, isFilled, color, colorFramed, color2);
+            rightLine.draw(board, isFilled, color, colorFramed, color2);
+            bottomLine.draw(board, isFilled, color, colorFramed, color2);
+        }
+
+    } else {
         return;
     }
 }
 
 void Rectangle::printInfo() {
-    std::cout << x << " " << y << " " << width << " " << height << std::endl;
+    std::cout << "rectangle " << x << " " << y << " "  << isFilled << " " << color << " " << colorFramed << " " << color2 << " " << width << " " << height << std::endl;
 }
 
 int Rectangle::getX() const {
@@ -41,5 +98,5 @@ int Rectangle::getHeight() const {
 }
 
 void Rectangle::saveInfo(std::ofstream &fout) {
-    fout << "rectangle" << " " << x << " " << y << " " << width << " " << height << std::endl;
+    fout << isFilled << " " << color << " " << colorFramed << " " << color2 << " " << "rectangle" << " " << x << " " << y << " " << width << " " << height << std::endl;
 }
--- src/Rectangle/Rectangle.h
+++ src/Rectangle/Rectangle.h
@@ -5,11 +5,11 @@
 
 class Rectangle : public Shape {
 private:
-    int x, y, width, height;
+    int width, height;
 public:
-    Rectangle(int x, int y, int width, int height);
+    Rectangle(int x, int y, bool isFilled, std::string& color, bool colorFramed, std::string& color2, int width, int height);
     ~Rectangle() override = default;
-    void draw(Board &board) override;
+    void draw(Board &board, bool isFilled, std::string color, bool colorFramed, std::string color2) override;
     void printInfo() override;
     void saveInfo(std::ofstream &fout) override;
     [[nodiscard]] int getX() const;
